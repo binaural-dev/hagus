a174c21708549ac47ed5d113ac8b29f001f7e729
Miguel Gozaine <miguel@binauraldev.com>
2022-01-18 15:27:30 -0400

    fix: Eliminando codigo que ya no es necesario referente a los productos "Negativo" y "Caucho".

--- custom/binaural_hagus_mods/models/product_template.py
+++ custom/binaural_hagus_mods/models/product_template.py
@@ -10,26 +10,6 @@ class ProductTemplate(models.Model):
         string="La categoria es Calcomanía o Etiqueta",
         compute="_compute_category_is_not_sticker")
 
-    @api.model
-    def create(self, vals):
-
-        """
-        Verificar que tanto "Negativo" como "Caucho" no se puedan crear dos veces.
-        Estos son productos generados al instalar el modulo que son necesarios para
-        el correcto funcionamiento del mismo.
-        """
-        if vals["name"].lower() == "negativo" and \
-                bool(self.env["product.template"].search([("name", '=', "Negativo")])):
-            raise ValidationError(
-                "Este producto ya existe, si quiere modificar su costo debe editarlo.")
-        if vals["name"].lower() == "caucho" and \
-                bool(self.env["product.template"].search([("name", '=', "Caucho")])):
-            raise ValidationError(
-                "Este producto ya existe, si quiere modificar su costo debe editarlo.")
-
-        res = super().create(vals)
-        return res
-
     @api.depends("categ_id.name")
     def _compute_category_is_not_sticker(self):
         for product in self:
