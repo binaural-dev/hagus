ab7f7c87215fd54c6d33224f27b4a7b773dfcca6
Miguel Gozaine <miguel@binauraldev.com>
2022-01-20 10:19:45 -0400

    fix: Agregar al campo MSI de product.product la capacidad de ser editado desde la linea de pedido en compra.

--- custom/binaural_hagus_mods/models/purchase.py
+++ custom/binaural_hagus_mods/models/purchase.py
@@ -4,5 +4,5 @@ from odoo import api, fields, models
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
-    msi = fields.Float(string="Medida MSI", digits=(14,6), related="product_id.msi")
-
+    msi = fields.Float(string="Medida MSI", digits=(
+        14, 6), related="product_id.msi", readonly=False, store_true=True)
