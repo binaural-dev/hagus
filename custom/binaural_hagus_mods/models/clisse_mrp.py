70b1b6d032415ad76e4a8c1be7d66f9034009e5b
Miguel Gozaine <miguel@binauraldev.com>
2022-01-21 16:04:39 -0400

    fix: Eliminados campos retazo y bobina en el tap de produccion del clisse que ya no son necesarios.

--- custom/binaural_hagus_mods/models/clisse_mrp.py
+++ custom/binaural_hagus_mods/models/clisse_mrp.py
@@ -26,8 +26,6 @@ class ClisseMrp(models.Model):
                            default=lambda self: fields.Date.today())
     coil_qty = fields.Integer(string="Cantidad de Bobinas")
     cutter = fields.Many2one("hr.employee", string="Cortador")
-    coil = fields.Many2one("product.template", string="Bobina")
-    scrap = fields.Float(string="Retazo", digits=(14, 2))
     estimate_msi = fields.Float(string="MSI Estimados", digits=(
         14, 2), compute="_compute_estimate_msi")
     consumed_msi = fields.Float(string="MSI Consumidos", digits=(14, 2))
