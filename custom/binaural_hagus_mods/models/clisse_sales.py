1f47ae887c4aa9d67b9718a98f468ff09722672b
Miguel Gozaine <miguel@binauraldev.com>
2022-01-25 15:26:12 -0400

    feat: Agregando campo de unidad de medida a la lista de materiales del clisse.

--- custom/binaural_hagus_mods/models/clisse_sales.py
+++ custom/binaural_hagus_mods/models/clisse_sales.py
@@ -386,3 +386,5 @@ class HagusClisseLines(models.Model):
     clisse_id = fields.Many2one('hagus.clisse', string='Clisse asociado')
     product_category_id = fields.Many2one(
         string="Categoría", related="product_id.categ_id", readonly=False, store_true=True)
+    product_uom_id = fields.Many2one(
+        string="Unidad", related="product_id.product_tmpl_id.uom_id")
