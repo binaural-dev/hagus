cd32fe5aa168d1af184b1aeefa62376f325e0972
Miguel Gozaine <miguel@binauraldev.com>
2022-02-15 11:32:22 -0400

    fix: Creando funcion inversa para poder seleccionar los msi directamente en la orden de compra de un producto de tipo bobina.

--- custom/binaural_hagus_mods/models/purchase.py
+++ custom/binaural_hagus_mods/models/purchase.py
@@ -6,7 +6,7 @@ class PurchaseOrderLine(models.Model):
 
     coil_cut_width_inches = fields.Float(related="product_id.cut_width_inches")
     msi = fields.Float(
-        string="Medida MSI", compute="_compute_msi")
+        string="Medida MSI", compute="_compute_msi", inverse="_inverse_msi")
     product_category_name = fields.Char(related="product_id.categ_id.name")
 
     @api.depends("product_qty", "coil_cut_width_inches")
@@ -15,3 +15,9 @@ class PurchaseOrderLine(models.Model):
         for order in self:
             if bool(order.product_qty) and bool(order.product_id.cut_width_inches):
                 order.msi = order.product_qty * order.coil_cut_width_inches * 0.012
+
+    def _inverse_msi(self):
+        self.product_qty = 0
+        for order in self:
+            if bool(order.msi) and bool(order.product_id.cut_width_inches):
+                order.product_qty = order.msi / (order.coil_cut_width_inches * 0.012)
